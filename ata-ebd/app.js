1aa31c2190c5e49b529bc519b62ab802492a9601
Luca Aguiar <lucanobre1@gmail.com>
2024-08-28 11:09:19 -0300

    W

--- ata-ebd/app.js
+++ ata-ebd/app.js
@@ -134,7 +134,7 @@ initializeDB('./databases/juv.db');
 initializeDB('./databases/manha.db');
 initializeDB('./databases/tarde.db');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
